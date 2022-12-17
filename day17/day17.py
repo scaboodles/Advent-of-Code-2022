07c3283a8814cde8bda5061bac686624db6f4719
scaboodles <owenj.wolff2@gmail.com>
2022-12-17 14:59:02 -0800

    day 17

---
+++ day17/day17.py
@@ -0,0 +1,87 @@
+class Rock:
+    def __init__(self, cave, rock_chars):
+        self.rock_lines = rock_chars.split("\n")
+        self.y = cave.highest_point() + 3 + self.height
+        self.x = 1
+
+    def is_stopped(self):
+        return self.y == self.height
+
+    def jet(self, dir):
+        match dir:
+            case "<":
+                self.x = max(self.x - 1, 0)
+            case ">":
+                self.x = min(self.x + 1, 6 - self.width)
+    
+    def drop(self):
+        self.y -=1 
+
+    def settle(self,cave):
+        return cave.add_occupied(self.get_occupied())
+    
+    def get_occupied(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.rock_lines[x][y] == "#":
+                    yield self.x + x, self.y + y
+
+
+    @property
+    def width(self):
+        return max(*map(lambda rl: len(rl), self.rock_lines))
+
+    @property
+    def height(self):
+        return len(self.rock_lines)
+
+class Cave:
+    def __init__(self):
+        self.grid = set()
+
+    def highest_point(self):
+        if len(self.grid):
+            return max(*map(lambda pos:pos[1], self.grid))
+        return 0
+
+    def add_occupied(self, positions):
+        self.grid.update(positions)
+
+def part_1(rock_input, jets):
+    rocks_chars = rock_input.split("\n\n")
+    print(rocks_chars)
+    cave = Cave()
+    jet_index = 0
+    for i in range(2022):
+        step = i%len(rocks_chars)
+        rock = Rock(cave, rocks_chars[step])
+        while not rock.is_stopped():
+            rock.jet(jets[jet_index])
+            rock.drop()
+            jet_index = (jet_index + 1) % len(jets)
+            
+        rock.settle(cave) 
+
+def get_rocks():
+    return """####
+    .#.
+    ###
+    .#.
+
+    ..#
+    ..#
+    ###
+
+    #
+    #
+    #
+    #
+
+    ##
+    ##
+    """
+
+def get_jets():
+    return ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+
+print(f"part 1 result: {part_1(get_rocks(), get_jets())}")
